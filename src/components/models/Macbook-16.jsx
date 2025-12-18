daa8f22bfa9e022ae1edd4c6bedc9060105ccb98
bhanu2006-24 <bhanupsaini2024@gmail.com>
2025-12-18 10:55:53 +0530

    still triing

--- src/components/models/Macbook-16.jsx
+++ src/components/models/Macbook-16.jsx
@@ -16,9 +16,9 @@ import { Color, SRGBColorSpace } from 'three';
 
 export default function MacbookModel16(props) {
     const { color } = useMacbookStore();
-  const { nodes, materials, scene} = useGLTF('/models/macbook-16-transformed.glb')
+  const { nodes, materials, scene} = useGLTF('./models/macbook-16-transformed.glb')
 
-    const texture = useTexture('/screen.png');
+    const texture = useTexture('./screen.png');
     texture.colorSpace = SRGBColorSpace;
     texture.needsUpdate = true;
 
@@ -59,4 +59,4 @@ export default function MacbookModel16(props) {
   )
 }
 
-useGLTF.preload('/models/macbook-16-transformed.glb')
+useGLTF.preload('./models/macbook-16-transformed.glb')
