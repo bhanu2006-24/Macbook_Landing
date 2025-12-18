ae480ceb2dbbe79d097ad60585d9613e77a170d8
bhanu2006-24 <bhanupsaini2024@gmail.com>
2025-12-18 10:58:51 +0530

    almost fixed the site

--- src/components/models/Macbook-14.jsx
+++ src/components/models/Macbook-14.jsx
@@ -16,9 +16,9 @@ import {Color, SRGBColorSpace} from 'three'
 
 export default function MacbookModel14(props) {
     const { color } = useMacbookStore();
-  const { nodes, materials, scene } = useGLTF('/models/macbook-14-transformed.glb');
+  const { nodes, materials, scene } = useGLTF('./models/macbook-14-transformed.glb');
 
-  const texture = useTexture('/screen.png');
+  const texture = useTexture('./screen.png');
     texture.colorSpace = SRGBColorSpace;
     texture.needsUpdate = true;
 
@@ -59,4 +59,4 @@ export default function MacbookModel14(props) {
   )
 }
 
-useGLTF.preload('/models/macbook-14-transformed.glb')
+useGLTF.preload('./models/macbook-14-transformed.glb')
